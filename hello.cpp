2657f6ea8ef1ff34d9e5402f283dbeb384a23614
Joel Renjith <98686646+joelrenjith@users.noreply.github.com>
2024-03-06 13:40:41 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "This is  Joel Renjith - PES1UG21CS247\n";
-  cout << "I have successfully built and run\n";
+  cout << "I have successfully built and run\n"
   return 0;
 }
